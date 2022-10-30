3864f4a0885c3b090d37476aeffde7a9b1989e6a
AnjanaThanguturi <anjanathanguturi@gmail.com>
2022-10-29 21:35:09 -0500

    changes

---
+++ src/main/java/com/anjanaClasses/DMS/Repository/DogRepository.java
@@ -0,0 +1,15 @@
+package com.anjanaClasses.DMS.Repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.anjanaClasses.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog,Integer>{
+
+	List<Dog> findByName(String name);
+	
+	
+
+}
---
+++ src/main/java/com/anjanaClasses/DMS/Repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.anjanaClasses.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.anjanaClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer,Integer>{
+
+}
